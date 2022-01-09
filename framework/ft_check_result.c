1fc49a6e86e7696379e756aca0bd083cb3c25d79
Adrian Weaver <aweaver@student.42.fr>
2022-01-09 20:28:48 +0100

    FIXED EVERYTHINGgit add *! Now need to work on test cases

--- framework/ft_check_result.c
+++ framework/ft_check_result.c
@@ -6,7 +6,7 @@
 /*   By: aweaver <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/09 16:42:01 by aweaver           #+#    #+#             */
-/*   Updated: 2022/01/09 17:10:04 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/01/09 20:27:49 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,14 @@ void	ft_check_result(int w_status, int *success)
 {
 	int	w_result;
 
+	w_result = 0;
 	if (WIFEXITED(w_status))
+	{
 		w_result = WEXITSTATUS(w_status);
+		if (w_result != 0)
+			w_result = 33;
+		w_result = 32;
+	}
 	else if (WIFSIGNALED(w_status))
 		w_result = WTERMSIG(w_status);
 	ft_print_result(w_result);
